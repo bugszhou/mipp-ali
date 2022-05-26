d2e88ae0b9f2e572ccca34949a3c0c68fc6b17ce
youzhao.zhou <youzhao.zhou@verystar.cn>
2022-05-26 21:01:10 +0800

    fix: 解决冲突

--- typings/aliapp/App.d.ts
+++ typings/aliapp/App.d.ts
@@ -1,51 +1,48 @@
-/**
- * App()相关的类型声明：
- *
- * 1. 生命周期方法声明(ILifetime)
- *
- * 2. 生命周期函数形参类型声明
- *
- */
-declare namespace IMippAliApp {
-  /**
-   * 生命周期方法:
-   *
-   * onLaunch(options: ILaunchOption): void
-   *
-   * onShow(options: ILaunchOption): void
-   *
-   * onHide(): void
-   *
-   * onError(error: string): void // 错误信息，包含堆栈
-   *
-   * onShareAppMessage(options: IOnShareAppMessageOptions) => IOnShareAppMessageResult
-   *
-   */
-  interface ILifetime extends Partial<tinyapp.IAppOptionsMethods> {}
+import {
+  IAppOnLaunchOptions,
+  IShareAppMessage,
+} from "../mini-types/global/types/lib.global";
 
+declare global {
   /**
-   * 形参声明
+   * App()相关的类型声明：
    *
-   * App中onLaunch和onShow事件的形参接口声明
-   * @example onLaunch(options: ILaunchOption): void
-   * @example onShow(options: ILaunchOption): void
+   * 1. 生命周期方法声明(ILifetime)
+   *
+   * 2. 生命周期函数形参类型声明
+   *
    */
-  interface ILaunchOption extends tinyapp.IAppLaunchOptions {}
+  namespace IMippAliApp {
+    /**
+     * 生命周期方法:
+     *
+     * onLaunch(options: ILaunchOption): void
+     *
+     * onShow(options: ILaunchOption): void
+     *
+     * onHide(): void
+     *
+     * onError(error: string): void // 错误信息，包含堆栈
+     *
+     * onShareAppMessage(options: IOnShareAppMessageOptions) => IOnShareAppMessageResult
+     *
+     */
+    type ILifetime = Partial<IAppOptions>;
 
-  /**
-   * 形参声明
-   *
-   * 全局分享配置
-   * @example onShareAppMessage(options: IOnShareAppMessageOptions) => IOnShareAppMessageResult
-   */
-  type IOnShareAppMessageOptions = tinyapp.OnShareAppMessageOptions;
-
-  /**
-   * 返回值声明
-   *
-   * App中监听系统主题改变事件(onThemeChange)事件的形参接口声明
-   * @example (result: OnThemeChangeCallbackResult) => void
-   */
-  interface IOnShareAppMessageResult
-    extends tinyapp.IOnShareAppMessageResult {}
+    /**
+     * 形参声明
+     *
+     * App中onLaunch和onShow事件的形参接口声明
+     * @example onLaunch(options: ILaunchOption): void
+     * @example onShow(options: ILaunchOption): void
+     */
+    type ILaunchOption = IAppOnLaunchOptions<{}>;
+    /**
+     * 返回值声明
+     *
+     * App中监听系统主题改变事件(onThemeChange)事件的形参接口声明
+     * @example (result: OnThemeChangeCallbackResult) => void
+     */
+    type IOnShareAppMessageResult = IShareAppMessage;
+  }
 }
--- typings/extends/global.d.ts
+++ typings/extends/global.d.ts
@@ -1,4 +1,9 @@
-import { IPageInstance } from "../mini-types/global/types/lib.global";
+import {
+  IAppOnLaunchOptions,
+  IAppOnUnhandledRejectionRes,
+  IPageInstance,
+  IShareAppMessage,
+} from "../mini-types/global/types/lib.global";
 
 declare global {
   /**
@@ -27,4 +32,36 @@ declare global {
    * @link https://opendocs.alipay.com/mini/framework/page-detail
    */
   function Page(opts: any): void;
+
+  export interface IAppOptions {
+    /**
+     * 生命周期回调：监听小程序初始化
+     * 当小程序初始化完成时触发，全局只触发一次
+     */
+    onLaunch<Query = {}>(options: IAppOnLaunchOptions<Query>): void;
+    /**
+     * 生命周期回调：监听小程序显示
+     * 当小程序启动，或从后台进入前台显示时触发
+     */
+    onShow<Query = {}>(options: IAppOnLaunchOptions<Query>): void;
+    /**
+     * 生命周期回调：监听小程序隐藏
+     * 当当前页面被隐藏时触发，例如跳转、按下设备 Home 键离开
+     */
+    onHide(): void;
+    /**
+     * 监听小程序错误
+     * 当小程序发生 js 错误时触发
+     */
+    onError(error: string): void;
+    /**
+     * 全局分享配置
+     */
+    onShareAppMessage(): IShareAppMessage;
+    /**
+     * 监听 unhandledrejection 事件
+     * 当 Promise 被 reject 且没有 reject 处理器时，会触发 onUnhandledRejection 事件
+     */
+    onUnhandledRejection(res: IAppOnUnhandledRejectionRes): void;
+  }
 }
