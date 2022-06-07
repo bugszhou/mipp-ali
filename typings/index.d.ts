e41ff9ce782278aab553d156b2ee08021823522c
youzhao.zhou <youzhao.zhou@verystar.cn>
2022-06-07 14:22:18 +0800

    feat: 导出方法

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,11 +1,9 @@
 /// <reference path="./mini-types/alipay/types/index.d.ts" />
 /// <reference path="./extends/global.d.ts" />
 /// <reference path="./aliapp/index.d.ts" />
-import { Base, MiniComponent } from "./Base/Base";
+export * from "./Base/Base";
 
 declare class PageBase<IData extends Record<string, any>> extends Base<IData> {
   constructor(options?: any);
   get componentName(): string;
 }
-
-export { MiniComponent };
