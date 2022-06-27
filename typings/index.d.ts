f3837aceade98f1ccca870c8fd84da106863f991
youzhao.zhou <youzhao.zhou@verystar.cn>
2022-06-27 15:54:49 +0800

    fix: 导出类型

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,12 +1,17 @@
 /// <reference path="./mini-types/alipay/types/index.d.ts" />
 /// <reference path="./extends/global.d.ts" />
 /// <reference path="./aliapp/index.d.ts" />
-import { Base, MiniComponent, method } from "./Base/Base";
+import {
+  Base,
+  MiniComponent,
+  method,
+  lifetimes,
+  pageLifetime,
+} from "./Base/Base";
 
 export class PageBase<IData extends Record<string, any>> extends Base<IData> {
   constructor(options?: any);
   get componentName(): string;
 }
 
-
-export { MiniComponent, method };
+export { MiniComponent, method, lifetimes, pageLifetime };
